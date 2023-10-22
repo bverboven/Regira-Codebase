8ec84ae0a120afea7d8927b1a11b9dba2b9f802a
bverboven <bramverboven@hotmail.com>
2023-10-23 00:23:47 +0200

    EntityManagerBase:EntityWrappingServiceBase

--- src/Common.Entities/Abstractions/EntityManagerBase.cs
+++ src/Common.Entities/Abstractions/EntityManagerBase.cs
@@ -1,5 +1,4 @@
-﻿using Regira.DAL.Paging;
-using Regira.Entities.Models.Abstractions;
+﻿using Regira.Entities.Models.Abstractions;
 
 namespace Regira.Entities.Abstractions;
 
@@ -14,7 +13,8 @@ public abstract class EntityManagerBase<TEntity, TSearchObject, TSortBy, TInclud
     {
     }
 }
-public abstract class EntityManagerBase<TEntity, TKey, TSearchObject, TSortBy, TIncludes> : IEntityManager<TEntity, TKey, TSearchObject, TSortBy, TIncludes>
+public abstract class EntityManagerBase<TEntity, TKey, TSearchObject, TSortBy, TIncludes> : EntityWrappingServiceBase<TEntity, TKey, TSearchObject, TSortBy, TIncludes>,
+    IEntityManager<TEntity, TKey, TSearchObject, TSortBy, TIncludes>
     where TEntity : class, IEntity<TKey>
     where TSearchObject : ISearchObject<TKey>, new()
     where TSortBy : struct, Enum
@@ -22,24 +22,8 @@ public abstract class EntityManagerBase<TEntity, TKey, TSearchObject, TSortBy, T
 {
     protected readonly IEntityRepository<TEntity, TKey, TSearchObject, TSortBy, TIncludes> Repo;
     protected EntityManagerBase(IEntityRepository<TEntity, TKey, TSearchObject, TSortBy, TIncludes> repo)
+        : base(repo)
     {
         Repo = repo;
     }
-
-    public virtual Task<TEntity?> Details(TKey id) => Repo.Details(id);
-    public virtual Task<IList<TEntity>> List(object? so = default, PagingInfo? pagingInfo = null) => Repo.List(so, pagingInfo);
-    public virtual Task<IList<TEntity>> List(IList<TSearchObject?> so, IList<TSortBy> sortBy, TIncludes? includes = null, PagingInfo? pagingInfo = null)
-        => Repo.List(so, sortBy, includes, pagingInfo);
-    public virtual Task<int> Count(object? so) => Repo.Count(so);
-    public virtual Task<int> Count(IList<TSearchObject?> so)
-        => Repo.Count(so);
-
-    public virtual Task Add(TEntity item) => Repo.Add(item);
-    public virtual Task Modify(TEntity item) => Repo.Modify(item);
-    public virtual Task Save(TEntity item) => Repo.Save(item);
-
-    public virtual Task Remove(TEntity item) => Repo.Remove(item);
-
-    public virtual Task<int> SaveChanges(CancellationToken token = default) => Repo.SaveChanges(token);
-
 }
