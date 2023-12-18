9ab210beb61abfce328a714e8f921edafc2cc513
bverboven <bramverboven@hotmail.com>
2023-12-18 09:29:33 +0100

    IdentityMailer

---
+++ src/Security.Authentication/Mail/IdentityMailer.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Regira.Office.Mail.Abstractions;
+using Regira.Office.Mail.Models;
+
+namespace Regira.Security.Authentication.Mail;
+
+public class IdentityMailer : IEmailSender
+{
+    private readonly IMailer _mailer;
+    public IdentityMailer(IMailer mailer)
+    {
+        _mailer = mailer;
+    }
+
+    public Task SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        var msg = new MessageObject
+        {
+            To = { new MailRecipient { Email = email } },
+            Subject = subject,
+            Body = htmlMessage
+        };
+        return _mailer.Send(msg);
+    }
+}
