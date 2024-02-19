7457e8b8ba1e0fff7383c90a66e1cc1a6af788e4
bverboven <bramverboven@hotmail.com>
2024-02-19 14:09:46 +0100

    inline file

--- src/Common.Web/Extensions/ControllerExtensions.cs
+++ src/Common.Web/Extensions/ControllerExtensions.cs
@@ -22,6 +22,6 @@ public static class ControllerExtensions
         var contentType = !string.IsNullOrWhiteSpace(file.ContentType)
             ? file.ContentType
             : ContentTypeUtility.GetContentType(file.FileName);
-        return ctrl.File(file.GetBytes()!, contentType, file.FileName);
+        return ctrl.File(file.GetBytes()!, contentType, inline ? null : file.FileName);
     }
 }
