5d4327d1a244f56540eb208c01f791968d2a37b2
bverboven <bramverboven@hotmail.com>
2024-02-27 17:05:33 +0100

    Moved to Regira.Services

--- tools/AzureBackupConsole/BackupOptions.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Regira.AzureBackupService;
-
-public class BackupOptions
-{
-    public string AzureConnectionString { get; set; } = null!;
-    public string BackupContainer { get; set; } = "backups";
-}
