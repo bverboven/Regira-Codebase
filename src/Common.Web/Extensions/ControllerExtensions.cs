b9a22bd8ff9c9a99bf69cd87be255cad60f733bb
bverboven <bramverboven@hotmail.com>
2024-08-26 17:01:39 +0200

    404 when null bytes for File

--- src/Common.Web/Extensions/ControllerExtensions.cs
+++ src/Common.Web/Extensions/ControllerExtensions.cs
@@ -8,21 +8,25 @@ namespace Regira.Web.Extensions;
 
 public static class ControllerExtensions
 {
-    public static FileContentResult File(this ControllerBase ctrl, INamedFile file, bool inline = true)
+    public static IActionResult File(this ControllerBase ctrl, INamedFile file, bool inline = true)
     {
         var disposition = new ContentDisposition
         {
             FileName = file.FileName,
             Inline = inline
         };
-        ctrl.Response.Headers.Add("Content-Disposition", disposition.ToString());
+        ctrl.Response.Headers["Content-Disposition"] = disposition.ToString();
         // make content-disposition available for axios client
-        ctrl.Response.Headers.Add("Access-Control-Expose-Headers", "Content-Disposition");
+        ctrl.Response.Headers["Access-Control-Expose-Headers"] = "Content-Disposition";
 
-        var bytes = file.GetBytes()!;
+        var bytes = file.GetBytes();
+        if (bytes == null)
+        {
+            return ctrl.NotFound();
+        }
         var contentType = !string.IsNullOrWhiteSpace(file.ContentType)
             ? file.ContentType
             : ContentTypeUtility.GetContentType(file.FileName) ?? ContentTypeUtility.GetContentType(bytes);
-        return ctrl.File(bytes!, contentType, inline ? null : file.FileName);
+        return ctrl.File(bytes, contentType, inline ? null : file.FileName);
     }
 }
