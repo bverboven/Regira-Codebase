800242e542ef35bb995035980d31d42fe5700405
bverboven <bramverboven@hotmail.com>
2024-12-07 22:54:28 +0100

    multi frameworks

--- src/Web.Swagger/Extensions/ApiControllerExtensions.cs
+++ src/Web.Swagger/Extensions/ApiControllerExtensions.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-#if NETSTANDARD2_0
-using Newtonsoft.Json.Converters;
-#else
+#if NET6_0_OR_GREATER
 using System.Text.Json.Serialization;
 #endif
 
@@ -9,14 +7,7 @@ namespace Regira.Web.Swagger.Extensions;
 
 public static class ApiControllerExtensions
 {
-#if NETSTANDARD2_0
-    public static IMvcCoreBuilder DisplayEnumAsString(this IMvcCoreBuilder builder)
-    {
-        var converter = new StringEnumConverter();
-        return builder
-            .AddJsonOptions(o => o.SerializerSettings.Converters.Add(converter));
-    }
-#else
+#if NET6_0_OR_GREATER
     public static IMvcBuilder DisplayEnumAsString(this IMvcBuilder builder)
     {
         return builder
