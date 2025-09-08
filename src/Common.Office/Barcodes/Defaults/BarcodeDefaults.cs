2ac55d222b88602ed3fe9b5e9e7c646065808c61
Bram Verboven <bram@regira.com>
2025-09-08 13:54:59 +0200

    add height

--- src/Common.Office/Barcodes/Defaults/BarcodeDefaults.cs
+++ src/Common.Office/Barcodes/Defaults/BarcodeDefaults.cs
@@ -7,7 +7,7 @@ namespace Regira.Office.Barcodes.Defaults;
 public static class BarcodeDefaults
 {
     public static BarcodeFormat Format { get; set; } = BarcodeFormat.Code128;
-    public static ImageSize Size { get; set; } = new(400, 50);
+    public static ImageSize Size { get; set; } = new(400, 100);
     public static Color Color { get; set; } = Color.Black;
     public static Color BackgroundColor { get; set; } = Color.White;
 }
